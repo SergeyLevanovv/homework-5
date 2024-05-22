aa296715be81cfdde855d7db57e609d3a4fd6301
SergeyLevanovv <ser.lewanow@gmail.com>
2024-05-22 04:16:26 +0400

    add Task-4.

---
+++ src/Task4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String input = scanner.nextLine();
+
+        String[] words = input.split(" ");
+
+        for (String word : words) {
+            if (startsWithVowel(word) && endsWithConsonant(word)) {
+                System.out.println(word);
+            }
+        }
+    }
+
+    public static boolean startsWithVowel(String word) {
+        char firstChar = Character.toLowerCase(word.charAt(0));
+        return firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u';
+    }
+
+    public static boolean endsWithConsonant(String word) {
+        char lastChar = Character.toLowerCase(word.charAt(word.length() - 1));
+        return !(lastChar == 'a' || lastChar == 'e' || lastChar == 'i' || lastChar == 'o' || lastChar == 'u');
+    }
+}
+
