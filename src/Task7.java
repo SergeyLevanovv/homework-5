feec03152d52548183d68b593d9a6f182857377e
SergeyLevanovv <ser.lewanow@gmail.com>
2024-05-22 04:22:41 +0400

    add Task-7.

---
+++ src/Task7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+
+        String input = scanner.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isUpperCase(c)) {
+                result += Character.toLowerCase(c);
+            } else if (Character.isLowerCase(c)) {
+                result += Character.toUpperCase(c);
+            } else if (Character.isDigit(c)) {
+                result += "_";
+            } else {
+                result += c;
+            }
+        }
+        System.out.println("Результат: " + result);
+    }
+}
+{
+}
